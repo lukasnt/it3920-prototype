2ff29af17c811b33bcba8fd16d7a12411be701f1
Lukas Tveiten <lukasnt@stud.ntnu.no>
2023-02-10 10:40:31 +0100

    Fix maven setup and create a simple spark app

--- src/main/scala/com/lukasnt/spark/App.scala
+++ src/main/scala/com/lukasnt/spark/App.scala
@@ -1,14 +1,19 @@
 package com.lukasnt.spark
 
+import org.apache.spark.sql.SparkSession
+
 /**
  * @author ${user.name}
  */
 object App {
   
-  private def foo(x : Array[String]): String = x.foldLeft("")((a, b) => a + b)
-  def main(args : Array[String]): Unit = {
-    println( "Hello World!" )
-    println("concat arguments = " + foo(args))
+  def main(args: Array[String]): Unit = {
+    val logFile = "./spark/README.md" // Should be some file on your system
+    val spark = SparkSession.builder.appName("Simple Applictaion").getOrCreate()
+    val logData = spark.read.textFile(logFile).cache()
+    val numAs = logData.filter(line => line.contains("a")).count()
+    val numBs = logData.filter(line => line.contains("b")).count()
+    println(s"Lines with a: $numAs, Lines with b: $numBs")
+    spark.stop()
   }
-
 }
--- src/test/scala/samples/AppTest.scala
+++ src/test/scala/samples/AppTest.scala
@@ -7,7 +7,7 @@ import Assert._
 class AppTest {
 
     @Test
-    def testOK() = assertTrue(true)
+    def testOK(): Unit = assertTrue(true)
 
 //    @Test
 //    def testKO() = assertTrue(false)
