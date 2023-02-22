24227eec1cf53da184ab908420832e54f33b573a
Lukas Tveiten <lukasnt@stud.ntnu.no>
2023-02-22 16:52:53 +0100

    Some clean up in comments

--- src/main/scala/com/lukasnt/spark/models/TemporalInterval.scala
+++ src/main/scala/com/lukasnt/spark/models/TemporalInterval.scala
@@ -3,7 +3,7 @@ package com.lukasnt.spark.models
 import java.time.temporal.{ChronoUnit, Temporal}
 
 /**
-  * TemporalInterval is an abstract class that represents a temporal interval.
+  * TemporalInterval is an class that represents a temporal interval.
   * @param startTime start time of the interval
   * @param endTime end time of the interval
   * @tparam T Temporal type of the interval (e.g. LocalDateTime, LocalTime, etc.)
@@ -12,17 +12,15 @@ class TemporalInterval[T <: Temporal](val startTime: T, val endTime: T)
     extends Serializable {
 
   /**
-    * Checks if this interval is after the given interval.
     * @param interval interval to compare with
-    * @return true if this interval is after the given interval, false otherwise
+    * @return true if this interval is equal the given interval, false otherwise
     */
   def equals(interval: TemporalInterval[T]): Boolean = {
-    this.startTime.equals(interval.startTime) && this.endTime
-      .equals(interval.endTime)
+    this.startTime.equals(interval.startTime) &&
+    this.endTime.equals(interval.endTime)
   }
 
   /**
-    * Checks if this interval meets the given interval.
     * @param interval interval to compare with
     * @return true if this interval meets the given interval, false otherwise
     */
@@ -31,7 +29,6 @@ class TemporalInterval[T <: Temporal](val startTime: T, val endTime: T)
   }
 
   /**
-    * Checks if this interval overlaps with the given interval.
     * @param interval interval to compare with
     * @return true if this interval overlaps the given interval, false otherwise
     */
@@ -40,7 +37,6 @@ class TemporalInterval[T <: Temporal](val startTime: T, val endTime: T)
   }
 
   /**
-    * Checks if this interval is before the given interval.
     * @param interval interval to compare with
     * @return true if this interval is before the given interval, false otherwise
     */
@@ -49,7 +45,6 @@ class TemporalInterval[T <: Temporal](val startTime: T, val endTime: T)
   }
 
   /**
-    * Checks if this interval is during the given interval.
     * @param interval interval to compare with
     * @return true if this interval is during the given interval, false otherwise
     */
@@ -59,24 +54,21 @@ class TemporalInterval[T <: Temporal](val startTime: T, val endTime: T)
   }
 
   /**
-    * Checks if this interval starts the given interval.
     * @param interval interval to compare with
     * @return true if this interval starts the given interval, false otherwise
     */
   def starts(interval: TemporalInterval[T]): Boolean = {
-    this.startTime.equals(interval.startTime) && this.endTime
-      .until(interval.endTime, ChronoUnit.NANOS) > 0
+    this.startTime.equals(interval.startTime) &&
+    this.endTime.until(interval.endTime, ChronoUnit.NANOS) > 0
   }
 
   /**
-    * Checks if this interval finishes the given interval.
     * @param interval interval to compare with
     * @return true if this interval finishes the given interval, false otherwise
     */
   def finishes(interval: TemporalInterval[T]): Boolean = {
-    this.startTime
-      .until(interval.startTime, ChronoUnit.NANOS) < 0 && this.endTime
-      .equals(interval.endTime)
+    this.startTime.until(interval.startTime, ChronoUnit.NANOS) < 0 &&
+    this.endTime.equals(interval.endTime)
   }
 
   override def toString: String = {
--- src/main/scala/com/lukasnt/spark/models/TemporalProperties.scala
+++ src/main/scala/com/lukasnt/spark/models/TemporalProperties.scala
@@ -2,19 +2,37 @@ package com.lukasnt.spark.models
 
 import java.time.temporal.Temporal
 
+/**
+  * Represents properties of a certain label-type with an temporal interval attached to it.
+  * This class is used to represent the vertex or edge properties of a temporal graph.
+  * @param interval valid interval of the properties-values
+  * @param typeLabel string indicating label-type
+  * @param properties properties with their values as a map of key-value pairs
+  * @tparam T Temporal type of the interval (e.g. LocalDateTime, LocalTime, etc.)
+  */
 class TemporalProperties[T <: Temporal](val interval: TemporalInterval[T],
                                         val typeLabel: String,
                                         val properties: Map[String, String])
     extends Serializable {
 
+  /**
+    * @return interval of the properties
+    */
   def getInterval: TemporalInterval[T] = {
     this.interval
   }
 
+  /**
+    * @return type-label of the properties
+    */
   def getTypeLabel: String = {
     this.typeLabel
   }
 
+  /**
+    * @param key key of the property
+    * @return value of the property with the given key
+    */
   def getPropertyValue(key: String): Serializable = {
     this.properties.get(key)
   }
@@ -26,14 +44,15 @@ class TemporalProperties[T <: Temporal](val interval: TemporalInterval[T],
   override def equals(obj: Any): Boolean = {
     obj match {
       case that: TemporalProperties[T] =>
-        this.interval.equals(that.interval) && this.properties
-          .equals(that.properties)
+        this.interval.equals(that.interval) &&
+          this.typeLabel.equals(that.typeLabel) &&
+          this.properties.equals(that.properties)
       case _ => false
     }
   }
 
   override def hashCode(): Int = {
-    val state = Seq(interval, properties)
+    val state = Seq(interval, typeLabel, properties)
     state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
   }
 }
