7c8aa55e59993de4833c504a3feb124bfb82f1bd
Lukas Tveiten <lukasnt@stud.ntnu.no>
2023-04-08 11:45:35 +0200

    Add zeppelin-notebooks commit

--- src/main/scala/com/lukasnt/spark/App.scala
+++ src/main/scala/com/lukasnt/spark/App.scala
@@ -10,22 +10,6 @@ object App {
 
   def main(args: Array[String]): Unit = {
     println("Hello World!")
-    Loggers.root.info("Hello World!")
-    Loggers.root.debug("Hello World!")
-    /*
-    val personGraph = SimpleSNBGraphs.personKnowsGraph()
-    val currentQuery = SimpleParameterQueries.interactionPaths(
-      topK = 25,
-      minLength = 3,
-      pathType = TemporalPathType.Continuous
-    )
-    val subgraph      = ParameterSubgraph(personGraph, currentQuery)
-    val weightedGraph = ParameterWeightMap(subgraph, currentQuery)
-    val pregelGraph   = ParameterPregel(weightedGraph, currentQuery)
-    val paths         = ParameterPathsConstruction(pregelGraph, currentQuery)
-    paths.foreach(println)
-     */
-    SimpleGraphX.run()
   }
 
 }
