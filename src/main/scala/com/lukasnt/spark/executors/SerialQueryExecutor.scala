359847062668c92e5afafccead28b53ace60df72
Lukas Tveiten <lukasnt@stud.ntnu.no>
2023-04-16 09:54:29 +0200

    Implement multiple single source serial bfs as a reference algorithm

--- src/main/scala/com/lukasnt/spark/executors/SerialQueryExecutor.scala
+++ src/main/scala/com/lukasnt/spark/executors/SerialQueryExecutor.scala
@@ -1,12 +1,107 @@
 package com.lukasnt.spark.executors
 
-import com.lukasnt.spark.models.Types.TemporalGraph
+import com.lukasnt.spark.models.TemporalPath
+import com.lukasnt.spark.models.Types._
 import com.lukasnt.spark.queries.{ParameterQuery, QueryResult}
+import org.apache.spark.graphx.{EdgeTriplet, VertexId}
 
 class SerialQueryExecutor extends ParameterQueryExecutor {
 
   def execute(parameterQuery: ParameterQuery, temporalGraph: TemporalGraph): QueryResult = {
-    ???
+    // Extract predicates as query is not serializable
+    val sourcePredicate: AttrVertex => Boolean      = parameterQuery.sourcePredicate
+    val destinationPredicate: AttrVertex => Boolean = parameterQuery.destinationPredicate
+
+    // Collect source and destination vertices
+    val sourceVertices: List[VertexId] = temporalGraph.vertices
+      .filter(v => sourcePredicate(AttrVertex(v)))
+      .map(_._1)
+      .collect()
+      .toList
+    val destinationVertices: List[VertexId] = temporalGraph.vertices
+      .filter(v => destinationPredicate(AttrVertex(v)))
+      .map(_._1)
+      .collect()
+      .toList
+
+    // For each source and destination vertex pair, run BFS
+    // and collect the top-k paths
+    QueryResult(
+      queriedGraph = temporalGraph,
+      pathEntries = sourceVertices
+        .flatMap(srcId => runBfs(temporalGraph, parameterQuery, srcId, destinationVertices))
+        .sortBy(_.weight)
+        .take(parameterQuery.topK)
+    )
   }
+
+  private def runBfs(graph: TemporalGraph,
+                     query: ParameterQuery,
+                     srcId: VertexId,
+                     destinationIds: List[VertexId]): List[SerialQueryExecutor.PathEntry] = {
+    var queue = List[SerialQueryExecutor.PathEntry]()
+    var paths = List[SerialQueryExecutor.PathEntry]()
+    queue = initQueue(graph, query, srcId)
+
+    // BFS
+    while (queue.nonEmpty) {
+      val current = queue.head
+      queue = queue.tail
+      val currentPath     = current.path
+      val currentInterval = current.interval
+      val currentWeight   = current.weight
+      val currentEndNode  = currentPath.endNode
+      val currentLength   = currentPath.length
+
+      // Found path to destination
+      if (destinationIds.contains(currentEndNode) && currentLength >= query.minLength && currentLength <= query.maxLength) {
+        paths = paths :+ current
+      }
+
+      //println(s"Current length: $currentLength")
+      // Extend current path
+      if (currentLength < query.maxLength) {
+        val outEdges = outTriplets(graph, currentEndNode)
+        val newPaths = outEdges
+          .filter(
+            edge => {
+              query.temporalPathType.validEdgeInterval(currentInterval, edge.attr.interval) &&
+              query.intermediatePredicate(AttrEdge(edge.srcId, edge.dstId, edge.attr))
+            }
+          )
+          .map(edge => {
+            val interval = query.temporalPathType.nextInterval(currentInterval, edge.attr.interval)
+            val weight   = query.weightMap(AttrEdge(edge.srcId, edge.dstId, edge.attr))
+            SerialQueryExecutor.PathEntry(currentPath :+ edge, interval, currentWeight + weight)
+          })
+        queue = queue ++ newPaths
+      }
+
+    }
+    paths
+  }
+
+  private def initQueue(graph: TemporalGraph,
+                        query: ParameterQuery,
+                        srcId: VertexId): List[SerialQueryExecutor.PathEntry] = {
+    val outEdges = outTriplets(graph, srcId)
+    outEdges.map(edge => {
+      val interval = query.temporalPathType.initInterval(edge.attr.interval)
+      val weight   = query.weightMap(AttrEdge(edge.srcId, edge.dstId, edge.attr))
+      SerialQueryExecutor.PathEntry(TemporalPath(edge), interval, weight)
+    })
+  }
+
+  private def outTriplets(graph: TemporalGraph, vertexId: VertexId): List[EdgeTriplet[Properties, Properties]] = {
+    graph.triplets.filter(_.srcId == vertexId).collect().toList
+  }
+
+}
+
+object SerialQueryExecutor {
+
+  def apply(): SerialQueryExecutor = new SerialQueryExecutor()
+
+  case class PathEntry(path: TemporalPath, interval: Interval, weight: Float)
 
 }
